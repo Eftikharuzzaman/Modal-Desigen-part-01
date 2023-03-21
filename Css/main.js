140a3d0d53bf9ad7d4123f7ff968b3c645e0eac9
Salman Rahaman Jony <jonyislamcmt@gmail.com>
2023-03-21 14:21:26 +0600

    changes js file and css file...check it..

--- Css/main.js
+++
@@ -1,9 +0,0 @@
-// let modal = document.querySelector('center')
-// document.getElementById('showModal').addEventListener('click',function(){
-//     modal.classList.add('showtotalmodal');
-// })
-
-// ShowTotalModal এর কোড এটা
-
-let close = document.querySelector('close');
-close
---
+++ main.js
@@ -0,0 +1,9 @@
+let modal = document.querySelector('.center');
+// changes code
+document.getElementById('showModal').addEventListener('click',function(){
+    modal.classList.toggle('showtotalmodal');
+    console.log('success adding class');
+})
+
+// ShowTotalModal এর কোড এটা
+
